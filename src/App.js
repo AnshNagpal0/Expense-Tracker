a648da1cd0e06a706d3a854e9f379fa02f88e021
Ansh <ansh03012004@gmail.com>
2024-01-07 00:22:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <container bg={'f8fafd'} maxW={'container.3xl'} height={'100vh'} p={'0'}>
+      <Flex height={'full'}>
+        <Box>
+
+
+          
+        </Box>
+      </Flex>
+    </container>
+  );
+}
+
+export default App;
